1a301ccde5d1d8affd486dc49214fea45454c099
damilurg <rootdamilurg@gmail.com>
2019-08-05 23:16:07 +0600

    compressed and corrected files

--- source/js/menu.js
+++ source/js/menu.js
@@ -10,7 +10,7 @@ const openMobileMenu = () => {
     menu.style.height = "335px"
     links.forEach((link, num) => setTimeout(() => link.style = "background-color: var(--detail-color); color: var(--detail-color); border-bottom: 1px solid var(--quaternary-color);", num * interval))
     links.forEach((link, num) => setTimeout(() => link.style = "background-color: var(--detail-color); color: var(--detail-color); border-bottom: 1px solid var(--detail-color);", (num + 1) * interval))
-    links.forEach((link, num) => setTimeout(() => link.style = "background-color: var(--quaternary-color); color: var(--detail-color); border-bottom: 1px solid var(--senary-color);", (num + 2) * interval))
+    links.forEach((link, num) => setTimeout(() => link.style = "background-color: var(--quaternary-color); color: var(--detail-color); border-bottom: 1px solid var(--secondary-color);", (num + 2) * interval))
 }
 
 // CLOSE MOBILE MENU EFFECT
@@ -47,3 +47,49 @@ const toggleHeaderOnScroll = window.onscroll = () => {
     else header.style.top = "-80px"
     prevScrollPos = currentScrollPos
 }
+
+//FORM VALIDATION
+const validateForm = () => {
+    const inName = document.getElementById("name");
+    const name = inName.value;
+    const nameRegExp = /^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$/;
+
+    const inEmail = document.getElementById("email");
+    const email = inEmail.value;
+    const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const inMessage = document.getElementById("message");
+    const message = inMessage.value;
+
+
+    if (nameRegExp.test(name) == false) {
+        inName.focus();
+        alert("Please, fill the name field correctly.");
+        inName.style = "outline: 2px solid red;";
+        return;
+    } else if (emailRegExp.test(email) == false) {
+        inEmail.focus();
+        inName.style = "outline: 2px solid green;";
+        alert("Please, fill the e-mail field correctly.");
+        inEmail.style = "outline: 2px solid red;";
+        return;
+    } else if (message == "") {
+        inMessage.focus();
+        inEmail.style = "outline: 2px solid green;";
+        inName.style = "outline: 2px solid green;";
+        alert("Please, fill the message field correctly.");
+        inMessage.style = "outline: 2px solid red;";
+        return;
+    } else {
+        alert("Message sent. Thank you for contacting us.");
+        inEmail.style = "outline: 2px solid var(--detail-color);";
+        inName.style = "outline: 2px solid  var(--detail-color);";
+        inMessage.style = "outline: 2px solid  var(--detail-color);";
+        inEmail.value = "";
+        inName.value = "";
+        inMessage.value = "";
+    }
+};
+
+const btSubmit = document.getElementById("submit");
+btSubmit.addEventListener("click", validateForm);
